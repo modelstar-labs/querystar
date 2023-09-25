e8ff33c30560d331914c840013daf064348a9ab0
Adithya Krishnan <krishsandeep@gmail.com>
2023-09-25 16:59:33 +0200

    Update: cli log

--- src/querystar/cli.py
+++ src/querystar/cli.py
@@ -12,7 +12,7 @@ from querystar.logger import initialize_logger
 
 
 @click.group()
-@click.version_option('0.3.10', message=f'\n{click.style("QueryStar", fg="magenta")}, installed version: {click.style("%(version)s", fg="magenta")}\n')
+@click.version_option('0.3.11', message=f'\n{click.style("QueryStar", fg="magenta")}, installed version: {click.style("%(version)s", fg="magenta")}\n')
 @click.pass_context
 def main(ctx):
     # ensure that ctx.obj exists and is a dict (in case `cli()` is called
@@ -44,7 +44,6 @@ def run(ctx, target: str, rl: bool):
 
     logger = logging.getLogger("querystar")
     logger.info(f"QueryStar running {target} (Press CTRL+C to quit)")
-    logger.info('Finished ACTION - slack.add_message')
 
     try:
         while True:
