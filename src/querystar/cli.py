5d63737c9db8484b0a1530b266073cc453c853e7
Adithya Krishnan <krishsandeep@gmail.com>
2023-10-03 11:58:25 +0200

    Bump: 0.3.14

--- src/querystar/cli.py
+++ src/querystar/cli.py
@@ -13,7 +13,7 @@ from querystar.logger import initialize_logger
 
 
 @click.group()
-@click.version_option('0.3.13', message=f'\n{click.style("QueryStar", fg="magenta")}, installed version: {click.style("%(version)s", fg="magenta")}\n')
+@click.version_option('0.3.14', message=f'\n{click.style("QueryStar", fg="magenta")}, installed version: {click.style("%(version)s", fg="magenta")}\n')
 @click.pass_context
 def main(ctx):
     # ensure that ctx.obj exists and is a dict (in case `cli()` is called
